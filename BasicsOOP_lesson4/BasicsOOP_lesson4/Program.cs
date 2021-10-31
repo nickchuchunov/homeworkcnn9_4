8584a80f7e5ed0e034abefadf4912e11526cf94b
nickchuchunov <nick.chuchunov@yandex.ru>
2021-10-31 18:30:15 +0300

    создание 4 практического задания

---
+++ BasicsOOP_lesson4/BasicsOOP_lesson4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOOP_lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        }
+    }
+}
